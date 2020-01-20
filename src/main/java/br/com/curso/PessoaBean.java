57f03234e2f72caa4cffec9a3042fe59df6857cd
Rafael Ramos <rafaelramos@sparkag.com.br>
2020-01-20 10:31:53 -0300

    Final do curso Introdução ao JSF

--- src/main/java/br/com/curso/PessoaBean.java
+++ src/main/java/br/com/curso/PessoaBean.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.component.html.HtmlCommandButton;
 
@@ -41,56 +40,24 @@ public class PessoaBean {
 	
 	private HtmlCommandButton commandButton;
 	private String nome;
-	private String sobrenome;
-	private String nomeCompleto;
+	private String senha;
+	private String texto;
 
 	private List<String> nomes = new ArrayList<String>();
 
 	public String addNome() {
-		setNomeCompleto(nome + " " + sobrenome);
 		
-		if (nomes.size() == 1) {
+		nomes.add(nome);
+		
+		if (nomes.size() == 2) {
 			
 			commandButton.setDisabled(true);
 			return "navegacao-dinamica?faces-redirect=true";
 		}
-		
-		nomes.add(nomeCompleto);
 
 		return ""; //null ou vazio fica na mesma página -> chamado de (outcome)
 	}
 
-	public String getNomeCompleto() {
-		return nomeCompleto;
-	}
-
-	public void setNomeCompleto(String nomeCompleto) {
-		this.nomeCompleto = nomeCompleto;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public List<String> getNomes() {
-		return nomes;
-	}
-
-	public void setNomes(List<String> nomes) {
-		this.nomes = nomes;
-	}
 
 	public HtmlCommandButton getCommandButton() {
 		return commandButton;
@@ -99,5 +66,47 @@ public class PessoaBean {
 	public void setCommandButton(HtmlCommandButton commandButton) {
 		this.commandButton = commandButton;
 	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	public String getTexto() {
+		return texto;
+	}
+
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+	
+	
 
 }
