25c5a413b05d476ab31a8c7fcf838ca8abd0706b
Rafael Ramos <rafaelramos@sparkag.com.br>
2020-01-28 15:23:50 -0300

    Criação do DAO Genérico

--- src/main/java/br/com/curso/PessoaBean.java
+++ src/main/java/br/com/curso/PessoaBean.java
@@ -1,11 +1,10 @@
 package br.com.curso;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.component.html.HtmlCommandButton;
+
+import br.com.dao.DAOGeneric;
+import br.com.entidade.Pessoa;
 
 /**
  * 
@@ -38,75 +37,30 @@ import javax.faces.component.html.HtmlCommandButton;
 @ManagedBean(name = "pessoaBean")
 public class PessoaBean {
 	
-	private HtmlCommandButton commandButton;
-	private String nome;
-	private String senha;
-	private String texto;
-
-	private List<String> nomes = new ArrayList<String>();
-
-	public String addNome() {
-		
-		nomes.add(nome);
-		
-		if (nomes.size() == 2) {
-			
-			commandButton.setDisabled(true);
-			return "navegacao-dinamica?faces-redirect=true";
-		}
-
-		return ""; //null ou vazio fica na mesma página -> chamado de (outcome)
-	}
-
-
-	public HtmlCommandButton getCommandButton() {
-		return commandButton;
-	}
-
-	public void setCommandButton(HtmlCommandButton commandButton) {
-		this.commandButton = commandButton;
-	}
-
-
-	public String getNome() {
-		return nome;
-	}
-
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-
-	public String getSenha() {
-		return senha;
-	}
-
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-
-	public String getTexto() {
-		return texto;
-	}
-
-
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
-
-
-	public List<String> getNomes() {
-		return nomes;
-	}
-
-
-	public void setNomes(List<String> nomes) {
-		this.nomes = nomes;
-	}
+	private Pessoa pessoa = new Pessoa();
+	private DAOGeneric<Pessoa> daoGeneric = new DAOGeneric<Pessoa>();
 	
+	public String salvar() {
+		daoGeneric.save(pessoa);
+		
+		return "";
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public DAOGeneric<Pessoa> getDaoGeneric() {
+		return daoGeneric;
+	}
+
+	public void setDaoGeneric(DAOGeneric<Pessoa> daoGeneric) {
+		this.daoGeneric = daoGeneric;
+	}
 	
 
 }
---
+++ src/main/java/br/com/dao/DAOGeneric.java
@@ -0,0 +1,21 @@
+package br.com.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.com.jpautil.JPAUtil;
+
+public class DAOGeneric<O> {
+	
+	public void save(O object) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		entityManager.persist(object);
+		
+		entityTransaction.commit();
+		entityManager.close();
+	}
+	
+}
